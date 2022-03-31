68a8d92d5c5181235eef6151da2b51591d3797a5
weerawichgithub <weerawich@gmail.com>
2022-03-31 15:23:20 -0400

    Update ListernersTestNG

--- src/test/java/pages/signinPage.java
+++ src/test/java/pages/signinPage.java
@@ -11,9 +11,24 @@ public class signinPage {
     public signinPage() {PageFactory.initElements(Driver.getDriver(),this);}
 
     @FindBy(id="email_create")
-    public WebElement emailInput;
+    public WebElement createEmailInput;
 
     @FindBy(xpath="//*[@id=\"SubmitCreate\"]/span")
     public WebElement createAccountBtn;
+
+    @FindBy(id="email")
+    public WebElement signinEmailInput;
+
+    @FindBy(id="passwd")
+    public WebElement signinPasswdInput;
+
+    @FindBy(id="SubmitLogin")
+    public WebElement signinBtn;
+
+    @FindBy(xpath="//*[@id=\"block_top_menu\"]/ul/li[2]/a")
+    public WebElement dressLink;
+
+    @FindBy(xpath="//*[@id=\"block_top_menu\"]/ul/li[2]/ul/li[2]/a")
+    public WebElement eveningDressesLink;
 
 }
